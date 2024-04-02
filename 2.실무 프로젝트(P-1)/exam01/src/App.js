0721a67fd2bcb74170705ef88314e51edb8f1cda
youngsan01 <intokys@gmail.com>
2024-04-02 19:24:32 +0900

    회원가입

---
+++ 2.실무 프로젝트(P-1)/exam01/src/App.js
@@ -0,0 +1,9 @@
+import MyComponent from "./components/MyComponent";
+
+const App = () => {
+  return <MyComponent name={15} num={3} />;
+};
+
+
+
+export default App;
---
+++ 2.실무 프로젝트(P-1)/exam01/src/components/MyComponent.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+const MyComponent = ({name, num}) => {
+        console.log(typeof num, num);
+    return (
+        <>
+         <div>안녕하세요!, 제 이름은 {name}입니다.</div>
+         <div>좋아하는 숫자는 {num}입니다.</div>
+        </>
+    );
+};
+
+MyComponent.defaultProps = {
+    // 기본값 설정
+    name: '기본 이름',
+}
+
+MyComponent.PropTypes = {
+    name: PropTypes.string;
+    num: PropTypes.number.isRequired,
+}
+
+export default MyComponent;
