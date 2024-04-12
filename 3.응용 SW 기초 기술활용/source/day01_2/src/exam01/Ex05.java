399d2b77164d34c76c27350b84a67f02af22f767
youngsan01 <intokys@gmail.com>
2024-04-12 16:20:13 +0900

    변수와 자료형 실습

---
+++ 3.응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex05.java
@@ -0,0 +1,17 @@
+package exam01;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        int num1 = 10;
+
+        num1 = 20;
+
+        final int num2 = 30; // 상수
+        //num2 = 40;
+
+        final int num3;
+        num3 = 30; //최초 한번만 상수는 대입 가능
+
+        System.out.println(num3);
+    }
+}
---
+++ 3.응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex10.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class Ex10 {
+    public static void main(String[] args) {
+       // byte num2 = ; // 명시적 형변환
+       // System.out.println(num2);
+
+        int num3 = 100;
+        int num4 = num3;
+
+        System.out.println(num4);
+    }
+}
